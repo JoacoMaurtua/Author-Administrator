85beb6cebf4ddf330de00ee56ab2bbe086c206d0
JoacoMaurtua <joacomaurtua@gmail.com>
2021-09-28 23:03:38 -0500

    handle onChange en el form

--- client/src/components/AuthorForm.js
+++ client/src/components/AuthorForm.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { Button, Card, CardBody, CardHeader, Form, FormGroup, Input, Label,Row,Col } from 'reactstrap';
 import {Link,useHistory} from 'react-router-dom';
 
 export default function AuthorForm({create,update}) {
+  const history = useHistory();
+
+  const homePage =e=>{
+    history.push('/');
+  }
+
+  const [input,setInput] = useState({
+    name:''
+  });
+
+  const handleOnChange =e=>{
+    const {name,value} = e.target.value;
+    setInput({
+      ...input,
+      [name]:value
+    })
+  }
+
   return (
     <div>
-      <Button color="link">Home</Button>
+      <Button color="link" onClick={homePage}>Home</Button>
       <h3>Agregue un nuevo autor:</h3>
       <Card style={{width: '35%', margin:'3rem auto'}}>
             <CardHeader>Formulario</CardHeader>
@@ -13,7 +31,13 @@ export default function AuthorForm({create,update}) {
                 <Form >
                     <FormGroup>
                         <Label for="tipo">Name : </Label>
-                        <Input type="text" id="tipo"/>
+                        <Input 
+                          type="text" 
+                          id="tipo"
+                          name="name"
+                          value={input.name}
+                          onChange={handleOnChange}
+                        /> 
                     </FormGroup>
                     <Row style={{marginTop:'1rem'}}>
                       <Col md={6}>
